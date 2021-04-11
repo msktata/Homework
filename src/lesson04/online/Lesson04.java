e81c41b333e439774e55b8387b39a4c02b56751f
msktata <86pretty@mail.ru>
2021-04-11 22:24:30 +0300

    Java.Level1
    Лекция 4

---
+++ src/lesson04/online/Lesson04.java
@@ -0,0 +1,53 @@
+package lesson04.online;
+
+public class Lesson04 {
+
+    public static void main(String[] args) {
+        Hero.voice();
+        System.out.println(Hero.countHead);
+//        Hero hero1 = new Hero("Mario", 15, 100, 50);
+//        hero1.getDamage();
+//        Hero hero2 = new Hero(150);
+
+//        System.out.println(Hero.Sword.name);
+
+//        Math math = new Math();
+//        Math.acos()
+
+//        System.out.println(hero1.getName());
+//        System.out.println(hero2.age);
+
+//        hero1.name = "Mario";
+//        hero1.setName("MarioTest");
+//        hero1.age = 15;
+//        hero1.health = 100;
+//        hero1.speed = 50;
+
+//        hero2.name = "King";
+//        hero2.age = 150;
+//        hero2.health = 1500;
+//        hero2.speed = -20;
+
+//        hero1 = hero2;
+//        hero2.age = 999;
+
+//        System.out.println("My name is " + hero1.name + ". I'm " + hero1.age + " years old");
+
+//        hero1.speak();
+//        hero1.run();
+//        hero1.healSelf(50);
+
+//        System.out.println("My name is " + hero2.name + ". I'm " + hero2.age + " years old");
+
+//        hero2.speak();
+//        hero2.run();
+//        hero2.healSelf(50);
+
+//        System.out.println("My name is " + hero1.getName() + ". I'm " + hero1.getAge() + " years old");
+
+//        hero1.speak();
+//        hero1.run();
+//        hero2.healSelf(50);
+    }
+
+}
